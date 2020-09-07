c0125077117389626f094b678b6528e29b1f30b7
peter <perter@jinghang.com>
2020-09-07 11:41:28 +0800

    first commit !!!!!

---
+++ src/main/java/com/jinghang/HelloWorld.java
@@ -0,0 +1,15 @@
+package com.jinghang;
+
+/**
+ * @Classname HelloWorld
+ * @Date 2020/9/7 11:39
+ * @Created by 周飞
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        System.out.println("hello world!");
+
+    }
+}
