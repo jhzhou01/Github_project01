fa05e3c71caae292505000c1f099258e67b3adbe
peter <perter@jinghang.com>
2020-09-07 15:52:18 +0800

    Test commit to hotfix

---
+++ src/main/java/com/jinghang/Test01.java
@@ -0,0 +1,12 @@
+package com.jinghang;
+
+/**
+ * @Classname Test01
+ * @Date 2020/9/7 15:51
+ * @Created by 周飞
+ */
+public class Test01 {
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}
